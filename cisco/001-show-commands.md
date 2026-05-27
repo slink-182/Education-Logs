ca22d182e1690ee61aa13edb070dee6dac97e4d9
slink-182 <slinkee182@gmail.com>
2026-05-27 15:19:45 -0600

    made test.py and cisco 001

---
+++ cisco/001-show-commands.md
@@ -0,0 +1,13 @@
+# 001 SHOW COMMANDS
+
+
+| Problem | Command |
+| :- | -: |
+| Port down	| show ip interface brief |
+| VLAN missing | show vlan brief |
+| Trunk missing	show | interfaces trunk |
+| Wrong port mode | show interfaces switchport |
+| VTP not syncing | show vtp status |
+| Wrong uplink | show cdp neighbors |
+| Traffic not learned | show mac address-table |
+| STP blocking | show spanning-tree |
--- python/test.py
+++ python/test.py
@@ -1,13 +1,24 @@
 # this file consists of python logic to debug code and chek that it works prior to recording it in the .md files
 
 # Convert between c and f 
-_choice = input("Would you like to input degrees in c or f: ")
+# _choice = input("Would you like to input degrees in c or f: ")
 
-if _choice == "c":
-    _degrees_c = float(input("celsius degrees: "))
-    _degrees_f = _degrees_c * (9/5) + 32
-if _choice == "f":
-    _degrees_f = float(input("fehrenheit degrees: "))
-    _degrees_c = (_degrees_f - 32) / (9/5)
+# if _choice == "c":
+#     _degrees_c = float(input("celsius degrees: "))
+#     _degrees_f = _degrees_c * (9/5) + 32
+# if _choice == "f":
+#     _degrees_f = float(input("fehrenheit degrees: "))
+#     _degrees_c = (_degrees_f - 32) / (9/5)
 
-print(f"c: {_degrees_c}\nf: {_degrees_f}")
+# print(f"c: {_degrees_c}\nf: {_degrees_f}")
+
+
+text = "this is a random setence containing numbers: 1234 5678"
+
+numbers = []
+
+for char in text:
+    if char.isdigit():
+        numbers.append(int(char))
+
+print(numbers)
