a75c7271c65ed0383e381a3043cdafd45ee48cda
slink-182 <slinkee182@gmail.com>
2026-06-07 01:26:18 -0600

    added to test.py

--- python/projects/test.py
+++ python/projects/test.py
@@ -12,13 +12,46 @@
 
 # print(f"c: {_degrees_c}\nf: {_degrees_f}")
 
+def NumberScrape():
+    text = "this is a random setence containing numbers: 1234 5678"
 
-text = "this is a random setence containing numbers: 1234 5678"
+    numbers = []
 
-numbers = []
+    for char in text:
+        if char.isdigit():
+            numbers.append(int(char))
 
-for char in text:
-    if char.isdigit():
-        numbers.append(int(char))
+    print(numbers)
 
-print(numbers)
+# NumberScrape()
+
+def NestedLoopTest():
+    for i in range(3):
+        '''in range, will not include the number listed. So a range of 3 will max at 2.'''
+        for j in range(3):
+            '''with each number in range, starting from 0, follow the same range but if i is equal to j, dont include.'''
+            if i == j:
+                break
+            print(i, j)
+'''
+potential numbers for the outer loop are 0,1,2.
+i = 0 and j = 0 break
+i = 1 and j = 0 print
+i = 2 and j = 0 print
+i = 2 and j = 1 print
+
+Cannot print if same value.
+'''
+NestedLoopTest()
+
+def BasicIf():
+    x = 10
+    if x < 5:
+        print("low")
+    else:
+        print("high")
+'''
+this will print high<3
+'''
+# BasicIf()
+
