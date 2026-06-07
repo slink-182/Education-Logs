b4ad370a182aff31761bbdd366f11d37f582baba
slink-182 <slinkee182@gmail.com>
2026-06-07 10:11:59 -0600

    made main() function select

--- python/projects/test.py
+++ python/projects/test.py
@@ -1,20 +1,39 @@
 # this file consists of python logic to debug code and chek that it works prior to recording it in the .md files
 
+LINE_BREAKS = 44
+
+# list of functios to call
+def main():
+    while (True):
+        c = int(input(f"{"-"*LINE_BREAKS}\nchoose function\n| 1. TempConversion\n| 2. NumberScrape\n| 3. NestedLoopTest\n| 4. BasicIf\n| 5. quit\n: "))
+        if c == 1:
+            TempConversion()
+        elif c == 2:
+            NumberScrape()
+        elif c == 3:
+            NestedLoopTest()
+        elif c == 4:
+            BasicIf()
+        elif c == 5:
+            break
+        elif c > 5 or c < 1 :
+            print("Choose a number between 1 and 5.")
+
+def TempConversion():
 # Convert between c and f 
-# _choice = input("Would you like to input degrees in c or f: ")
+    _choice = input("Would you like to input degrees in c or f: ")
 
-# if _choice == "c":
-#     _degrees_c = float(input("celsius degrees: "))
-#     _degrees_f = _degrees_c * (9/5) + 32
-# if _choice == "f":
-#     _degrees_f = float(input("fehrenheit degrees: "))
-#     _degrees_c = (_degrees_f - 32) / (9/5)
+    if _choice == "c":
+        _degrees_c = float(input("celsius degrees: "))
+        _degrees_f = _degrees_c * (9/5) + 32
+    if _choice == "f":
+        _degrees_f = float(input("fehrenheit degrees: "))
+        _degrees_c = (_degrees_f - 32) / (9/5)
 
-# print(f"c: {_degrees_c}\nf: {_degrees_f}")
+    print(f"c: {_degrees_c}\nf: {_degrees_f}")
 
 def NumberScrape():
     text = "this is a random setence containing numbers: 1234 5678"
-
     numbers = []
 
     for char in text:
@@ -22,8 +41,6 @@ def NumberScrape():
             numbers.append(int(char))
 
     print(numbers)
-
-# NumberScrape()
 
 def NestedLoopTest():
     for i in range(3):
@@ -42,7 +59,6 @@ i = 2 and j = 1 print
 
 Cannot print if same value.
 '''
-NestedLoopTest()
 
 def BasicIf():
     x = 10
@@ -53,5 +69,6 @@ def BasicIf():
 '''
 this will print high<3
 '''
-# BasicIf()
 
+# call main to select other functions!
+main()
